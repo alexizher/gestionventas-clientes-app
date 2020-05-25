f19d04316c3182f2df0bc6e26aad535359891ef0
Alexis Herrera <alexizher@gmail.com>
2020-05-25 17:32:32 -0500

    Create factura.service.ts
    implementación del services para factura

---
+++ src/app/facturas/services/factura.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { URL_BACKEND } from 'src/app/config/config';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Factura } from '../models/factura';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FacturaService {
+
+  private urlEndPoint: string = URL_BACKEND + '/api/facturas';
+
+  constructor(private http: HttpClient) { }
+
+  getFactura(id: number): Observable<Factura> {
+    return this.http.get<Factura>(`${this.urlEndPoint}/${id}`);
+  }
+  
+}
